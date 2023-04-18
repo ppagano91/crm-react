3b928aa56c0d4c2cb6ebe2c5de7306c5194ff7d3
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-18 14:23:32 -0300

    Resaltar página actual

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -10,13 +12,19 @@ const Layout = () => {
         </h2>
         <nav className="mt-10">
           <Link
-            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            className={`${
+              location.pathname === "/" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/"
           >
             Clientes
           </Link>
           <Link
-            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            className={`${
+              location.pathname === "/clientes/nuevo"
+                ? "text-blue-300"
+                : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/clientes/nuevo"
           >
             Nuevo Cliente

c33e2f0bb5d1908b4abff2e4ffda1b5c768012cf
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-18 14:30:38 -0300

    Creando función loader()

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import NuevoCliente from "./pages/NuevoCliente";
-import Index from "./pages/Index";
+import Index, { loader as clientesLoader } from "./pages/Index";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
+        loader: clientesLoader,
       },
       {
         path: "/clientes/nuevo",
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+// El "loader" ejecuta cuando el componente cargue: IDEAL para cargar un State o consultar una API y obtener un resultado para mostrar en el componente
+export function loader() {
+  return "Loader";
+}
+
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus Clientes</p>
+    </>
+  );
 };
 
 export default Index;

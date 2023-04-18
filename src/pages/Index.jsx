6a5a839ac57f5e2424964c2cb499e815c0048e34
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-18 16:11:49 -0300

    Obtener datos de Loader con useLoaderData

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,51 @@
+import { useLoaderData } from "react-router-dom";
 import React from "react";
 
 // El "loader" ejecuta cuando el componente cargue: IDEAL para cargar un State o consultar una API y obtener un resultado para mostrar en el componente
 export function loader() {
-  return "Loader";
+  const clientes = [
+    {
+      id: 1,
+      nombre: "Juan",
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 2,
+      nombre: "Karen",
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 3,
+      nombre: "Josue",
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 4,
+      nombre: "Miguel",
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 5,
+      nombre: "Pedro",
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+  ];
+  return clientes;
 }
 
 const Index = () => {
+  const clientes = useLoaderData();
+  console.log(clientes);
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>

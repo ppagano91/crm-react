bed6cc9ffcc54136b3fbf6f1b4d87417adb0814f
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-19 09:49:41 -0300

    Validación + Componente Error

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useActionData } from "react-router-dom";
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 /*
   Equivalente a una petición POST
@@ -8,12 +9,27 @@ import Formulario from "../components/Formulario";
 export async function action({ request }) {
   const formData = await request.formData();
   const datos = Object.fromEntries(formData);
-  console.log(datos);
+
+  const errores = [];
+  // Validando Formulario
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios");
+  }
+
+  // Retornar datos si hay errores
+  if (Object.keys(errores).length) {
+    return errores;
+  }
+
   return null;
 }
 
 const NuevoCliente = () => {
   const navigate = useNavigate();
+  const errores = useActionData();
+
+  console.log(errores);
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -29,6 +45,8 @@ const NuevoCliente = () => {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        {errores?.length &&
+          errores.map((error, i) => <Error key={i}>{error}</Error>)}
         <Form method="post">
           <Formulario />
           <input

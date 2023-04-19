21a6cfd67537a779dcb84f76f54dd46b08e5b981
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-19 08:43:18 -0300

    nuevo componente Cliente + mostrar Info de clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import React from "react";
+import Clientes from "../components/Cliente";
 
 // El "loader" ejecuta cuando el componente cargue: IDEAL para cargar un State o consultar una API y obtener un resultado para mostrar en el componente
 export function loader() {
@@ -50,6 +51,24 @@ const Index = () => {
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tus Clientes</p>
+      {clientes.length > 0 ? (
+        <table className="w-full bg-white mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Clientes</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente cliente={cliente} key={cliente.id} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10"></p>
+      )}
     </>
   );
 };

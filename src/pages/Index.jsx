8ae747cd66fe31af574189dc5a266b3abea2e399
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-20 08:25:41 -0300

    Consumiendo datos de json-server

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,49 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import React from "react";
 import Cliente from "../components/Cliente";
+import { obtenerClientes } from "../data/clientes";
 
 /*
   El "loader" ejecuta cuando el componente cargue: IDEAL para cargar un State o consultar una API y obtener un resultado para mostrar en el componente
   La función loader() es como una petición GET
 */
 export function loader() {
-  const clientes = [
-    {
-      id: 1,
-      nombre: "Juan",
-      telefono: 102013313,
-      email: "juan@juan.com",
-      empresa: "Codigo Con Juan",
-    },
-    {
-      id: 2,
-      nombre: "Karen",
-      telefono: 138198313,
-      email: "karen@juan.com",
-      empresa: "Codigo Con Juan",
-    },
-    {
-      id: 3,
-      nombre: "Josue",
-      telefono: 31983913,
-      email: "josue@juan.com",
-      empresa: "Codigo Con Juan",
-    },
-    {
-      id: 4,
-      nombre: "Miguel",
-      telefono: 319381983,
-      email: "miguel@juan.com",
-      empresa: "Codigo Con Juan",
-    },
-    {
-      id: 5,
-      nombre: "Pedro",
-      telefono: 1398198938,
-      email: "pedro@juan.com",
-      empresa: "Codigo Con Juan",
-    },
-  ];
+  const clientes = obtenerClientes();
+
   return clientes;
 }
 

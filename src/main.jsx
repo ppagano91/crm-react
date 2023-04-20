678fd8c0e05fae191ba339c279f619e5b6a0eb0d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-20 08:42:42 -0300

    Agrego hook useRouteError de react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import NuevoCliente, {
   action as nuevoClienteAction,
 } from "./pages/NuevoCliente";
 import Index, { loader as clientesLoader } from "./pages/Index";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Index />,
         loader: clientesLoader,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/clientes/nuevo",
